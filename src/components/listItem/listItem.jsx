04b8bab8defb4ccdaefa4d603e6baca438b8d366
ChihaAdam <chihaadam137@gmail.com>
2025-03-23 09:07:27 +0100

    added lazy loading 2

--- src/components/listItem/listItem.jsx
+++ src/components/listItem/listItem.jsx
@@ -1,7 +1,8 @@
 import TodoControl from '../todoControl/todoControl';
-import UpdateComponent from '../updateComponent/updateComponent';
 import styles from './listItem.module.css'
 import {useState} from 'react'
+import { Suspense,lazy } from 'react';
+const UpdateComponent = lazy(()=>import("../updateComponent/updateComponent"));
 function ListItem({todo,moveUp,moveDown,remove,index,last}) {
   const [showMore,setShowMore]=useState(false);
   return (
@@ -12,7 +13,11 @@ function ListItem({todo,moveUp,moveDown,remove,index,last}) {
         <p className={styles.subInfo}>• created on : {todo.creationDate.date}</p>
         <p className={styles.subInfo}>• at : {todo.creationDate.time}</p>
         {
-          showMore ? <UpdateComponent discard={()=>setShowMore(false)} index={index} /> : null
+          showMore ? 
+          <Suspense fallback={<div>loading ...</div>}>
+            <UpdateComponent discard={()=>setShowMore(false)} index={index} />
+          </Suspense> 
+          : null
         }
       </div>
         <TodoControl disabledDown={index==last}
